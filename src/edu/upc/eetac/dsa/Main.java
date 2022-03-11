3df57a8444c71f0cadf9d08b9a3e49d7bc73c0c5
ad-qu <adquiros15@outlook.com>
2022-03-11 15:59:58 +0100

    for update

--- src/edu/upc/eetac/dsa/Main.java
+++ src/edu/upc/eetac/dsa/Main.java
@@ -8,10 +8,10 @@ public class Main
     {
         Figura[] f = new Figura[4];
 
-        f[0] = new Circulo(5);
-        f[1] = new Cuadrado(10);
-        f[2] = new Rectangulo(7,7);
-        f[3] = new Triangulo(3,3);
+        f[0] = new Circulo(5); //78,5375
+        f[1] = new Cuadrado(10); //100
+        f[2] = new Rectangulo(8,7); //56
+        f[3] = new Triangulo(3,3); //4,5
 
         System.out.println("Primero tenemos: " + Arrays.toString(f));
 
@@ -20,7 +20,7 @@ public class Main
         System.out.println("Ordenamos y nos queda: " + Arrays.toString(f));
 
         double sum = 0;
-        for(int i = 0; i < 4; i++) { sum = f[i].Area() + sum; i++; }
+        for(int i = 0; i < 4; i++) { sum = f[i].Area() + sum; }
 
         System.out.println("El área es de " + sum);
     }
